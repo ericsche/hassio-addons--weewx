7e4233666e90866b85a515da2404871366ca2f47
Eric Scherlinger <eric.scherlinger@live.fr>
2023-11-21 23:29:20 +0100

    fix bug

--- weewx/run.sh
+++ weewx/run.sh
@@ -22,6 +22,10 @@ export ZIGBEE2MQTT_DATA="$(bashio::config 'data_path')"
 if ! bashio::fs.file_exists "$ZIGBEE2MQTT_DATA/weewx.conf"; then
     mkdir -p "$ZIGBEE2MQTT_DATA" || bashio::exit.nok "Could not create $ZIGBEE2MQTT_DATA"
 
+bashio::log.info "Copy default config..."
+cp /etc/weewx/weewx.conf $ZIGBEE2MQTT_DATA/weewx.conf
+
+bashio::log.info "Reconfigure..."
 /home/weewx/bin/wee_config --reconfigure --driver=$DRIVER --latitude=$LATITUDE --longitude=$LONGITUDE --altitude=$ALTITUDE,$ALTITUDEUNIT --location=$LOCATION --units=$UNITS --no-prompt --config=$DATA_PATH/weewx.conf
 
 fi
@@ -29,14 +33,14 @@ fi
 sed -i '/INSERT_SERVER_URL_HERE/ a \
 \ \ \ \ \ \ \ \ topic = weather\
 \ \ \ \ \ \ \ \ unit_system = US\
-' /home/weewx/weewx.conf
+' $ZIGBEE2MQTT_DATA/weewx.conf
 
-sed -i 's/INSERT_SERVER_URL_HERE/mqtt:\/\/'$MQTTUSER':'$MQTTPASSWORD'@core-mosquitto:1883/g' /home/weewx/weewx.conf
+sed -i 's/INSERT_SERVER_URL_HERE/mqtt:\/\/'$MQTTUSER':'$MQTTPASSWORD'@core-mosquitto:1883/g' $ZIGBEE2MQTT_DATA/weewx.conf
 
-sed -i 's/archive_interval = 300/archive_interval = 60/g' /home/weewx/weewx.conf
+sed -i 's/archive_interval = 300/archive_interval = 60/g' $ZIGBEE2MQTT_DATA/weewx.conf
 
-sed -i 's/log_success = True/log_success = False/g' /home/weewx/weewx.conf
-sed -i 's/week_start = 6/week_start = 0/g' /home/weewx/weewx.conf
+sed -i 's/log_success = True/log_success = False/g' $ZIGBEE2MQTT_DATA/weewx.conf
+sed -i 's/week_start = 6/week_start = 0/g' $ZIGBEE2MQTT_DATA/weewx.conf
 
 bashio::log.info "Starting Weewx..."
 /home/weewx/bin/weewxd $ZIGBEE2MQTT_DATA/weewx.conf
