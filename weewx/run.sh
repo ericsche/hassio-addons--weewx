a61335e8187de23dae65ecdef41280bb20d9d9a5
Eric Scherlinger <eric.scherlinger@live.fr>
2023-12-05 23:22:18 +0100

    path update

--- weewx/run.sh
+++ weewx/run.sh
@@ -23,30 +23,25 @@ if ! bashio::fs.file_exists "$ZIGBEE2MQTT_DATA/weewx.conf"; then
     mkdir -p "$ZIGBEE2MQTT_DATA" || bashio::exit.nok "Could not create $ZIGBEE2MQTT_DATA"
 
 bashio::log.info "Copy default config..."
-cp /home/weewx/weewx.conf $ZIGBEE2MQTT_DATA/weewx.conf
+cp /etc/weewx/weewx.conf $ZIGBEE2MQTT_DATA/weewx.conf
 
 bashio::log.info "Reconfigure..."
-/home/weewx/bin/wee_config --reconfigure --driver=$DRIVER --latitude=$LATITUDE --longitude=$LONGITUDE --altitude=$ALTITUDE,$ALTITUDEUNIT --location=$LOCATION --units=$UNITS --no-prompt --config=/home/weewx/weewx.conf
+/usr/bin/wee_config --reconfigure --driver=$DRIVER --latitude=$LATITUDE --longitude=$LONGITUDE --altitude=$ALTITUDE,$ALTITUDEUNIT --location=$LOCATION --units=$UNITS --no-prompt --config=$DATA_PATH/weewx.conf
 
 fi
 
-#sed -i '/INSERT_SERVER_URL_HERE/ a \
-#\ \ \ \ \ \ \ \ topic = weather\
-#\ \ \ \ \ \ \ \ unit_system = US\
-#' $ZIGBEE2MQTT_DATA/weewx.conf
+sed -i '/INSERT_SERVER_URL_HERE/ a \
+\ \ \ \ \ \ \ \ topic = weather\
+\ \ \ \ \ \ \ \ unit_system = US\
+' $ZIGBEE2MQTT_DATA/weewx.conf
 
-#sed -i 's/INSERT_SERVER_URL_HERE/mqtt:\/\/'$MQTTUSER':'$MQTTPASSWORD'@core-mosquitto:1883/g' $ZIGBEE2MQTT_DATA/weewx.conf
+sed -i 's/INSERT_SERVER_URL_HERE/mqtt:\/\/'$MQTTUSER':'$MQTTPASSWORD'@core-mosquitto:1883/g' $ZIGBEE2MQTT_DATA/weewx.conf
 
-sed -i 's/archive_interval = 300/archive_interval = 60/g' /home/weewx/weewx.conf
+sed -i 's/archive_interval = 300/archive_interval = 60/g' $ZIGBEE2MQTT_DATA/weewx.conf
 
-
-sed -i 's/log_success = True/log_success = False/g' /home/weewx/weewx.conf
-
-sed -i 's/week_start = 6/week_start = 0/g' /home/weewx/weewx.conf
-
-sed -i 's/device_type = acurite-bridge/device_type = wu-client/g' /home/weewx/weewx.conf
-
-cp /home/weewx/weewx.conf $ZIGBEE2MQTT_DATA/weewx.conf
+sed -i 's/log_success = True/log_success = False/g' $ZIGBEE2MQTT_DATA/weewx.conf
+sed -i 's/week_start = 6/week_start = 0/g' $ZIGBEE2MQTT_DATA/weewx.conf
 
 bashio::log.info "Starting Weewx..."
-/home/weewx/bin/weewxd /home/weewx/weewx.conf
+
+/usr/bin/weewxd $ZIGBEE2MQTT_DATA/weewx.conf
