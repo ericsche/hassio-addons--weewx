e932ec43b999dbf0b09f7842942ea9eb3447c2ab
Eric Scherlinger <eric.scherlinger@live.fr>
2024-08-14 15:16:28 +0200

    Try update to weewX5

--- weewx/run.sh
+++ weewx/run.sh
@@ -18,34 +18,32 @@ MQTTUSER="$(jq --raw-output '.mqttUser' $CONFIG_PATH)"
 MQTTPASSWORD="$(jq --raw-output '.mqttPassword' $CONFIG_PATH)"
 
 
-export ZIGBEE2MQTT_DATA="$(bashio::config 'data_path')"
-if ! bashio::fs.file_exists "$ZIGBEE2MQTT_DATA/weewx.conf"; then
-    mkdir -p "$ZIGBEE2MQTT_DATA" || bashio::exit.nok "Could not create $ZIGBEE2MQTT_DATA"
+export WEEWX_DATA="$(bashio::config 'data_path')"
+if ! bashio::fs.file_exists "$WEEWX_DATA/weewx.conf"; then
+    mkdir -p "$WEEWX_DATA" || bashio::exit.nok "Could not create $WEEWX_DATA"
 
-bashio::log.info "Copy default config..."
-cp /etc/weewx/weewx.conf $ZIGBEE2MQTT_DATA/weewx.conf
-
+bashio::log.info "Create default config..."
+/usr/share/weewx/weectl station create --driver=$DRIVER --latitude=$LATITUDE --longitude=$LONGITUDE --altitude=$ALTITUDE,$ALTITUDEUNIT --location=$LOCATION --units=$UNITS --no-prompt --config=$WEEWX_DATA/weewx.conf --sqlite-root=$WEEWX_DATA/archive --html-root=$WEEWX_DATA/public_html  --skin-root=$WEEWX_DATA/skins  --no-prompt
 
 fi
+/usr/share/weewx/weectl station reconfigure --driver=$DRIVER --latitude=$LATITUDE --longitude=$LONGITUDE --altitude=$ALTITUDE,$ALTITUDEUNIT --location=$LOCATION --units=$UNITS --no-prompt --config=$WEEWX_DATA/weewx.conf --sqlite-root=$WEEWX_DATA/archive --html-root=$WEEWX_DATA/public_html  --skin-root=$WEEWX_DATA/skins  --no-prompt
 
-bashio::log.info "Reconfigure..."
-/usr/bin/wee_config --reconfigure --driver=$DRIVER --latitude=$LATITUDE --longitude=$LONGITUDE --altitude=$ALTITUDE,$ALTITUDEUNIT --location=$LOCATION --units=$UNITS --no-prompt --config=$ZIGBEE2MQTT_DATA/weewx.conf
+
 
 
 sed -i '/INSERT_SERVER_URL_HERE/ a \
 \ \ \ \ \ \ \ \ topic = weather\
 \ \ \ \ \ \ \ \ unit_system = METRICWX\
-' $ZIGBEE2MQTT_DATA/weewx.conf
+' $WEEWX_DATA/weewx.conf
 
-sed -i 's/INSERT_SERVER_URL_HERE/mqtt:\/\/'$MQTTUSER':'$MQTTPASSWORD'@core-mosquitto:1883/g' $ZIGBEE2MQTT_DATA/weewx.conf
+sed -i 's/INSERT_SERVER_URL_HERE/mqtt:\/\/'$MQTTUSER':'$MQTTPASSWORD'@core-mosquitto:1883/g' $WEEWX_DATA/weewx.conf
 
-sed -i 's/archive_interval = 300/archive_interval = 60/g' $ZIGBEE2MQTT_DATA/weewx.conf
+sed -i 's/archive_interval = 300/archive_interval = 60/g' $WEEWX_DATA/weewx.conf
 
-sed -i 's/log_success = True/log_success = False/g' $ZIGBEE2MQTT_DATA/weewx.conf
-sed -i 's/week_start = 6/week_start = 0/g' $ZIGBEE2MQTT_DATA/weewx.conf
+sed -i 's/log_success = True/log_success = False/g' $WEEWX_DATA/weewx.conf
+sed -i 's/week_start = 6/week_start = 0/g' $WEEWX_DATA/weewx.conf
 
-sed -i 's/SQLITE_ROOT = %(WEEWX_ROOT)s/archive/QLITE_ROOT ='$ZIGBEE2MQTT_DATA'/weewx.conf/g' $ZIGBEE2MQTT_DATA/weewx.conf
 
 bashio::log.info "Starting Weewx..."
 
-/usr/bin/weewxd $ZIGBEE2MQTT_DATA/weewx.conf
+/usr/share/weewx/weewxd --config=$WEEWX_DATA/weewx.conf
