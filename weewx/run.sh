24e013b387d7f2bcaee527a74a4a81d3f97c8a80
Eric Scherlinger <eric.scherlinger@live.fr>
2026-04-12 15:16:47 +0200

    Remove broken neowx-material sed injection (already in image config from build)

--- weewx/run.sh
+++ weewx/run.sh
@@ -44,20 +44,6 @@ fi
 # Force correct port in [Ecowittcustom] (persistent config may have old value)
 sed -i '/^\[Ecowittcustom\]/,/^\[/{s/port = .*/port = 8083/}' "$WEEWX_CONF"
 
-# --- Ensure neowx-material skin report is configured ---
-if ! grep -q 'neowx-material' "$WEEWX_CONF"; then
-    bashio::log.info "Adding neowx-material report section..."
-    # Insert after the HTML_ROOT line inside [StdReport]
-    sed -i '/^\[StdReport\]/,/^\[/{
-        /HTML_ROOT = public_html/a\
-\    [[neowx-material]]\
-\        skin = neowx-material\
-\        enable = true\
-\        lang = fr\
-\        unit_system = metricwx
-    }' "$WEEWX_CONF"
-fi
-
 # --- Apply station settings ---
 sed -i "s/station_type = Simulator/station_type = Ecowittcustom/g" "$WEEWX_CONF"
 sed -i "s/latitude = .*/latitude = $LATITUDE/g" "$WEEWX_CONF"
